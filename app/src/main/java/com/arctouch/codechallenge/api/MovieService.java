53064b3bc940cfdd8e95e9e8da4d7e69ebf26367
Fabio Luiz <luiz.fabio@gmail.com>
2018-03-07 03:36:47 -0300

    Task 3 complete
    * Remove all logic from HomeActivity, leaving on it just the necessary to update the UI. All the network and treatment logic was placed on the MovieService.

--- app/src/main/java/com/arctouch/codechallenge/api/MovieService.java
+++ app/src/main/java/com/arctouch/codechallenge/api/MovieService.java
@@ -1,7 +1,20 @@
 package com.arctouch.codechallenge.api;
 
+import android.view.View;
+
+import com.arctouch.codechallenge.data.Cache;
+import com.arctouch.codechallenge.home.HomeAdapter;
+import com.arctouch.codechallenge.interfaces.MovieCallbackInterface;
+import com.arctouch.codechallenge.interfaces.UpcomingMoviesCallbackInterface;
+import com.arctouch.codechallenge.model.Genre;
+import com.arctouch.codechallenge.model.Movie;
+import com.arctouch.codechallenge.model.UpcomingMoviesResponse;
 import com.arctouch.codechallenge.util.Constants;
 
+import java.util.ArrayList;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -13,11 +26,39 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MovieService {
 
-    public static final TmdbApi api = new Retrofit.Builder()
+    private static final TmdbApi api = new Retrofit.Builder()
             .baseUrl(Constants.URL)
             .client(new OkHttpClient.Builder().build())
             .addConverterFactory(MoshiConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
             .create(TmdbApi.class);
+
+
+    public static void getMovie(int movieId, MovieCallbackInterface mci) {
+        api.movie((long) movieId, Constants.API_KEY, Constants.DEFAULT_LANGUAGE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(movieFound -> {
+                    mci.onGetMovieSuccess(movieFound);
+                });
+    }
+
+    public static void getUpcomingMovies(int page, UpcomingMoviesCallbackInterface umci) {
+        api.upcomingMovies(Constants.API_KEY, Constants.DEFAULT_LANGUAGE, (long) page, Constants.DEFAULT_REGION)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(moviesList -> {
+                    for (Movie movie : moviesList.results) {
+                        movie.genres = new ArrayList<>();
+                        for (Genre genre : Cache.getGenres()) {
+                            if (movie.genreIds.contains(genre.id)) {
+                                movie.genres.add(genre);
+                            }
+                        }
+                    }
+
+                    umci.onGetUpcomingMoviesSuccess(moviesList.results);
+                });
+    }
 }
--- app/src/main/java/com/arctouch/codechallenge/content/movie/MovieActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/content/movie/MovieActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import com.arctouch.codechallenge.R;
 import com.arctouch.codechallenge.api.MovieService;
+import com.arctouch.codechallenge.interfaces.MovieCallbackInterface;
+import com.arctouch.codechallenge.model.Movie;
 import com.arctouch.codechallenge.util.Constants;
 import com.arctouch.codechallenge.util.MovieImageUrlBuilder;
 import com.bumptech.glide.Glide;
@@ -48,32 +50,33 @@ public class MovieActivity extends Activity {
             movieId = bundle.getInt(Constants.MOVIE_ID_KEY);
         } catch (Exception e) {
             Log.w(TAG, "onCreate: no valid movie Id given", e);
+            movieId = 0;
         }
 
-        MovieService.api.movie((long) movieId, Constants.API_KEY, Constants.DEFAULT_LANGUAGE)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    titleTextView.setText(response.title);
-                    genresTextView.setText(TextUtils.join(", ", response.genres));
-                    releaseDateTextView.setText(response.releaseDate);
-                    overviewTextView.setText(response.overview);
-                    String posterPath = response.posterPath;
-                    String backdropPath = response.backdropPath;
+        if(movieId > 0){
+            Activity thisActivity = this;
+            MovieService.getMovie(movieId, movie -> {
+                titleTextView.setText(movie.title);
+                genresTextView.setText(TextUtils.join(", ", movie.genres));
+                releaseDateTextView.setText(movie.releaseDate);
+                overviewTextView.setText(movie.overview);
+                String posterPath = movie.posterPath;
+                String backdropPath = movie.backdropPath;
 
-                    if (!TextUtils.isEmpty(posterPath)) {
-                        Glide.with(this)
-                                .load(movieImageUrlBuilder.buildPosterUrl(posterPath))
-                                .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
-                                .into(posterImageView);
-                    }
+                if (!TextUtils.isEmpty(posterPath)) {
+                    Glide.with(thisActivity)
+                            .load(movieImageUrlBuilder.buildPosterUrl(posterPath))
+                            .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                            .into(posterImageView);
+                }
 
-                    if (!TextUtils.isEmpty(backdropPath)) {
-                        Glide.with(this)
-                                .load(movieImageUrlBuilder.buildBackdropUrl(backdropPath))
-                                .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
-                                .into(backdropImageView);
-                    }
-                });
+                if (!TextUtils.isEmpty(backdropPath)) {
+                    Glide.with(thisActivity)
+                            .load(movieImageUrlBuilder.buildBackdropUrl(backdropPath))
+                            .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                            .into(backdropImageView);
+                }
+            });
+        }
     }
 }
